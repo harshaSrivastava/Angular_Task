884fe6f73485ee087139f484c2efdf62a32744b4
harshaSrivastava <70124450+harshaSrivastava@users.noreply.github.com>
2021-06-21 21:56:27 +0530

    Update python.py

--- python.py
+++ python.py
@@ -1,49 +1,3 @@
-# # from datetime import datetime
-
-# # # datetime object containing current date and time
-# # now = datetime.now()
- 
-# # # print("now =", now)
-
-# # # # dd/mm/YY H:M:S
-# # dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
-# # print("date and time =", dt_string)	
-# from datetime import date
-
-# today = date.today()
-# print("Today's date:", today)
-# # importing whole module
-# from tkinter import *
-# from tkinter.ttk import *
-
-# # importing strftime function to
-# # retrieve system's time
-# from time import strftime
-
-# # creating tkinter window
-# root = Tk()
-# root.title('Clock')
-
-# # This function is used to
-# # display time on the label
-# def time():
-# 	string = strftime('%H:%M:%S %p')
-# 	lbl.config(text = string)
-# 	lbl.after(1000, time)
-
-# # Styling the label widget so that clock
-# # will look more attractive
-# lbl = Label(root, font = ('calibri', 40, 'bold'),
-# 			background = 'purple',
-# 			foreground = 'white')
-
-# # Placing clock at the centre
-# # of the tkinter window
-# lbl.pack(anchor = 'center')
-# time()
-
-# mainloop()
-
 from tkinter import *
 from tkinter import ttk
 from tkinter import font
@@ -63,7 +17,6 @@ def clock_time():
     root.after(1000,clock_time)
 
 root =Tk()
-# root.attributes("-fullscreen", False)
 root.configure(bg= "black")
 root.bind("x",quit)
 root.geometry("1200x400")
